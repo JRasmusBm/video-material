c12496a8beebc59a602da2353088f7ac84d70fb3
jrasmusbm <JRasmusBm@gmail.com>
2024-05-30 21:11:55 +0200

    docs: add comments to setup file

--- original-snippets/setup.sh
+++ original-snippets/setup.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 
+# Step 1: Create a docker container
 docker run --name=snippets -it ubuntu:20.04 /bin/bash
 # exit
 # docker start -ai snippets
 # docker rm snippets
 
+# Step 2: Set up Sudo
 apt update -y;
 apt install -y sudo;
 rm -rf /var/lib/apt/lists/*;
-passwd; # Create a sudo password
+passwd; # Manual step - Create a sudo password
 
-adduser snippets; # Create a user
+# Step 3: Create the user
+adduser snippets; # Manual Step - Password + Optional User Info 
 usermod -a -G tty snippets
 usermod -a -G sudo snippets;
-su - snippets; # Enter the user password you made
+su - snippets;
 
-sudo apt update -y;
+# Step 4: Install dependencies
+sudo apt update -y; # Manual step - Enter sudo password
 sudo apt install -y git neovim curl ripgrep;
 
-mkdir -p .config/git
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+. "$HOME/.nvm/nvm.sh" 
 
-# rm -rf projects/dotfiles
+nvm install 18
+nvm use 18
+npm i -g typescript
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+bash ~/.fzf/install # Some manual options
+source ~/.fzf.bash
+
+# Install dotfiles
 git clone https://github.com/JRasmusBm/dotfiles projects/dotfiles
 
 cd projects/dotfiles
@@ -33,16 +46,8 @@ git submodule update --init --recursive
 git submodule deinit -f vim/pack/python/start/vimpyter
 
 # Install Dotfiles
+mkdir -p .config/git
 python3 scripts/symlink.py
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
-
-nvm install 18
-nvm use 18
-npm i -g typescript
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-source ~/.fzf.bash
-
 cd ~/projects
+nvim
